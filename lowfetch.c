816073279561105b9185481be404e91171b72bf3
callyral <125226346+callyral@users.noreply.github.com>
2023-10-21 19:53:57 -0300

    Add shell information

--- lowfetch.c
+++ lowfetch.c
@@ -34,6 +34,7 @@ enum SystemChars
     CHAR_ASCII = 'a',
     CHAR_DISTRO_ID = 'd',
     CHAR_KERNEL_VERSION = 'k',
+    CHAR_SHELL = 's',
     CHAR_UPTIME = 'u'
 };
 
@@ -42,6 +43,7 @@ struct SystemInfo
     char *ascii;
     char *distro_id;
     char *kernel_version;
+    char *shell;
     char *uptime;
 };
 
@@ -130,19 +132,23 @@ int main(int argc, char **argv)
         }
     }
 
+    /* requires freeing */
     char *ascii = get_ascii(use_ascii_file, ascii_filename, ASCII_FILESIZE);
     char *distro_id = get_distro_id(DISTRO_ID_SIZE);
-    char *uptime = get_uptime(UPTIME_SIZE);
     char *kernel_version = get_kernel_version(kernel_version_shorten, KERNEL_VERSION_SIZE);
+    char *uptime = get_uptime(UPTIME_SIZE);
 
-    struct SystemInfo system_info = {.ascii = ascii, .distro_id = distro_id, .kernel_version = kernel_version, .uptime = uptime};
+    /* does not require freeing */
+    char *shell = get_shell();
+
+    struct SystemInfo system_info = {.ascii = ascii, .distro_id = distro_id, .kernel_version = kernel_version, .shell = shell, .uptime = uptime};
 
     info_print(accent_color_char, accent_bold, use_order_file, order_filename, ORDER_FILESIZE, system_info);
     
     free(ascii);
     free(distro_id);
-    free(uptime);
     free(kernel_version);
+    free(uptime);
     return 0;
 }
 
@@ -275,6 +281,50 @@ char *get_distro_id(size_t size)
     return distro_id;
 }
 
+char *get_kernel_version(bool shorten, size_t size)
+{
+    FILE *file = fopen("/proc/version", "r");
+    if (!file)
+    {
+        fprintf(stderr, "error: '/proc/version' (kernel information) is unreadable\n");
+        return NULL;
+    }
+
+    char *kernel_version;
+    kernel_version = malloc((size+1)*sizeof(*kernel_version));
+    if (!shorten)
+    {
+        fgets(kernel_version, size, file);
+        kernel_version[strlen(kernel_version) - 1] = 0; // trim the traling newline
+    }
+    else
+    {
+        char *temp = malloc((size+1)*sizeof(*temp));
+        for (int i = 0; i < 3; ++i)
+        {
+            fscanf(file, "%s", temp);
+            sprintf(temp, "%s ", temp);
+            strcat(kernel_version, temp);
+        }
+    }
+    
+    fclose(file);
+
+    return kernel_version;
+}
+
+char *get_shell()
+{
+    char *shell = getenv("SHELL");
+    if (!shell)
+    {
+        fprintf(stderr, "error: $SHELL is unknown\n");
+        return NULL;
+    }
+
+    return shell;
+}
+
 char *get_uptime(size_t size)
 {
     FILE *file = fopen("/proc/uptime", "r");
@@ -314,38 +364,6 @@ char *get_uptime(size_t size)
     return uptime;
 }
 
-char *get_kernel_version(bool shorten, size_t size)
-{
-    FILE *file = fopen("/proc/version", "r");
-    if (!file)
-    {
-        fprintf(stderr, "error: '/proc/version' (kernel information) is inaccessible\n");
-        return NULL;
-    }
-
-    char *kernel_version;
-    kernel_version = malloc((size+1)*sizeof(*kernel_version));
-    if (!shorten)
-    {
-        fgets(kernel_version, size, file);
-        kernel_version[strlen(kernel_version) - 1] = 0; // trim the traling newline
-    }
-    else
-    {
-        char *temp = malloc((size+1)*sizeof(*temp));
-        for (int i = 0; i < 3; ++i)
-        {
-            fscanf(file, "%s", temp);
-            sprintf(temp, "%s ", temp);
-            strcat(kernel_version, temp);
-        }
-    }
-    
-    fclose(file);
-
-    return kernel_version;
-}
-
 int info_print(char accent_color_char, bool accent_bold, bool use_order_file, char *order_filename, size_t order_filesize, struct SystemInfo system_info)
 {
     char *ansi_accent_color = get_ansi_color_from(accent_color_char, accent_bold);
@@ -353,12 +371,12 @@ int info_print(char accent_color_char, bool accent_bold, bool use_order_file, ch
     order = malloc((order_filesize+1)*sizeof(*order));
     if (!use_order_file)
     {
-        sprintf(order, "%c%c%c%c", CHAR_ASCII, CHAR_DISTRO_ID, CHAR_UPTIME, CHAR_KERNEL_VERSION);
+        sprintf(order, "%c%c%c%c%c", CHAR_ASCII, CHAR_DISTRO_ID, CHAR_SHELL, CHAR_UPTIME, CHAR_KERNEL_VERSION);
     }
     else if (access(order_filename, R_OK) == -1)
     {
         fprintf(stderr, "error: '%s' is unreadable or doesn't exist\n", order_filename);
-        sprintf(order, "%c%c%c%c", CHAR_ASCII, CHAR_DISTRO_ID, CHAR_UPTIME, CHAR_KERNEL_VERSION);
+        sprintf(order, "%c%c%c%c%c", CHAR_ASCII, CHAR_DISTRO_ID, CHAR_SHELL, CHAR_UPTIME, CHAR_KERNEL_VERSION);
     }
     else
     {
@@ -370,10 +388,13 @@ int info_print(char accent_color_char, bool accent_bold, bool use_order_file, ch
         switch (order[i]) {
             case CHAR_ASCII:          printf("%s%s%s\n",         ansi_accent_color, system_info.ascii, ANSI_COLOR_RESET); break;
             case CHAR_DISTRO_ID:      printf("%sdistro:%s %s\n", ansi_accent_color, ANSI_COLOR_RESET,  system_info.distro_id); break;
-            case CHAR_UPTIME:         printf("%suptime:%s %s\n", ansi_accent_color, ANSI_COLOR_RESET,  system_info.uptime); break;
-            case CHAR_KERNEL_VERSION: printf("%skernel:%s %s\n", ansi_accent_color, ANSI_COLOR_RESET,  system_info.kernel_version);
+            case CHAR_KERNEL_VERSION: printf("%skernel:%s %s\n", ansi_accent_color, ANSI_COLOR_RESET,  system_info.kernel_version); break;
+            case CHAR_SHELL:          printf("%sshell:%s %s\n",  ansi_accent_color, ANSI_COLOR_RESET,  system_info.shell); break;
+            case CHAR_UPTIME:         printf("%suptime:%s %s\n", ansi_accent_color, ANSI_COLOR_RESET,  system_info.uptime);
         }
     }
+
+    free(order);
 
     return 0;
 }
