0437e61417dd5a05ec01cc9ffe87652df1c1e8a4
callyral <125226346+callyral@users.noreply.github.com>
2023-11-03 19:06:10 -0300

    Separate uptime file

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = clang
 CFLAGS = -std=c99 -fuse-ld=mold -Wall -O3
 DEBUGFLAGS = -std=c99 -fuse-ld=mold -Wall -g -O0
-SRC = src/lowfetch.c src/include/lowfetch_base.c src/include/package_amount.c src/include/kernel_version.c src/include/argument_parsing.c
+SRC = src/lowfetch.c src/include/lowfetch_base.c src/include/package_amount.c src/include/kernel_version.c  src/include/uptime.c src/include/argument_parsing.c
 
 all: build run
 
--- src/lowfetch.c
+++ src/lowfetch.c
@@ -11,6 +11,7 @@
 #include "include/kernel_version.h"
 #include "include/lowfetch_base.h"
 #include "include/argument_parsing.h"
+#include "include/uptime.h"
 #include "include/sizes.h"
 
 /* from https://www.asciiart.eu/animals/cats */
@@ -194,45 +195,6 @@ char *get_shell()
     }
 
     return shell;
-}
-
-char *get_uptime(size_t size)
-{
-    FILE *file = fopen("/proc/uptime", "r");
-    if (!file)
-    {
-        fprintf(stderr, "error: uptime is inaccessible\n");
-        return NULL;
-    }
-
-    char *uptime;
-    unsigned int uptime_seconds = -1; // default value is integer limit, making errors obvious
-    uptime = malloc((size+1)*sizeof(*uptime));
-
-    fscanf(file, "%u", &uptime_seconds); // store value of file to an unsigned integer, flooring (rounding) it
-    // format uptime string accordingly
-    if (uptime_seconds >= 60)
-    {
-        switch (uptime_seconds%60) // switch seconds
-        {
-            case 0:
-                sprintf(uptime, "%u minutes", uptime_seconds/60);
-                break;
-            case 1:
-                sprintf(uptime, "%u minutes and 1 second", uptime_seconds/60);
-                break;
-            default:
-                sprintf(uptime, "%u minutes and %u seconds", uptime_seconds/60, uptime_seconds%60);
-        }
-    }
-    else
-    {
-        sprintf(uptime, "%u seconds", uptime_seconds);
-    }
-
-    fclose(file);
-
-    return uptime;
 }
 
 char *get_xdg_desktop() {
